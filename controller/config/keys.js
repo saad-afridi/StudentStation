a8c0ac0d33a00a37d53ee50b2c43c8a7efbde794
Zereck12 <saadafridi2001@gmail.com>
2021-07-01 16:32:59 -0400

    Added usermodel and userrouter and hid config info

--- controller/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	mongoUrl:
-		'mongodb+srv://admin:admin@cluster0.b25cu.mongodb.net/StudentStation?retryWrites=true&w=majority',
-};
--- server.js
+++ server.js
@@ -2,20 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+require('dotenv').config()
 // const users = require('./controller/routes/api/users');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 // Express middleware
 app.use(cors());
 app.use(express.json);
 
 // Db Config and connect
-const db = require('./controller/config/keys').mongoUrl;
+const db = process.env.MONGO_ATLAS_URI
 mongoose
 	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 	.then(() => console.log('Successfully connected to MongoDB'))
 	.catch((err) => console.log(err));
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Servers up and running on ${port}`));
+const userRouter = require('./controller/routes/api/users')
+
+app.use('./users', userRouter)
+
+app.listen(port, () => console.log(`Servers up and running on: ${port}`));
